ea79c360e551c5f6e6a72a38968bf2905659f31e
Samukelisiwe <ST10489024@rcconnect.edu.za>
2026-07-01 08:58:59 +0200

    created the home page with basic layout

---
+++ jscript.js
@@ -0,0 +1,53 @@
+let shape = document.getElementById("shape");
+
+let colors = ["red", "pink", "green", "black", "purple"];
+let colorIndex = 0;
+
+let isCircle = false;
+let size = 150;
+
+// Button 1
+function changeColor() {
+    colorIndex++;
+
+    if (colorIndex >= colors.length) {
+        colorIndex = 0;
+    }
+
+    shape.style.backgroundColor = colors[colorIndex];
+}
+
+// Button 2
+function changeShape() {
+
+    if (isCircle) {
+        shape.style.borderRadius = "0%";
+        isCircle = false;
+    } else {
+        shape.style.borderRadius = "50%";
+        isCircle = true;
+    }
+
+}
+
+// Button 3
+function increaseSize() {
+    size += 20;
+
+    shape.style.width = size + "px";
+    shape.style.height = size + "px";
+}
+
+// Button 4
+function resetShape() {
+
+    size = 150;
+    colorIndex = 0;
+    isCircle = false;
+
+    shape.style.width = "150px";
+    shape.style.height = "150px";
+    shape.style.backgroundColor = "red";
+    shape.style.borderRadius = "0%";
+
+}
